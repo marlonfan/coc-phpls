1600a7964a5b69c466714fd3114920a0fce49f87
Marlon Fan <marlon@tfan.net>
2019-01-20 16:52:10 +0800

    fix top namespace function bugs

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,72 @@
-import { ExtensionContext, LanguageClient, ServerOptions, workspace, services, TransportKind, LanguageClientOptions, FileSystemWatcher } from 'coc.nvim'
-import { DocumentSelector } from 'vscode-languageserver-protocol'
-import { CancellationToken } from 'vscode-jsonrpc'
-import Uri from 'vscode-uri';
-import Glob from 'glob'
+import {
+  ExtensionContext,
+  LanguageClient,
+  ServerOptions,
+  workspace,
+  services,
+  TransportKind,
+  LanguageClientOptions,
+  FileSystemWatcher,
+  CompletionContext,
+  ProvideCompletionItemsSignature,
+  ProviderResult
+} from "coc.nvim";
+import {
+  DocumentSelector,
+  CompletionItem,
+  TextDocument,
+  Position,
+  CompletionList,
+  DidOpenTextDocumentNotification
+} from "vscode-languageserver-protocol";
+import { CancellationToken } from "vscode-jsonrpc";
+import Uri from "vscode-uri";
+import Glob from "glob";
 
-import { WorkspaceDiscovery } from './workspaceDiscovery';
+import { WorkspaceDiscovery } from "./workspaceDiscovery";
+import { CompleterResult } from "readline";
 
-const sections = ['php']
+const sections = ["php"];
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  let { subscriptions } = context
-  let c = workspace.getConfiguration()
-  const config = c.get('phpls') as any
-  const enable = config.enable
-  const file = require.resolve('intelephense-server');
+  let { subscriptions } = context;
+  let c = workspace.getConfiguration();
+  const config = c.get("phpls") as any;
+  const enable = config.enable;
+  const file = require.resolve("intelephense-server");
 
-  if (enable === false) return
+  if (enable === false) return;
   if (!file) {
-    workspace.showMessage("intelephense-server not found!, please run yarn global add intelephense-server", 'error')
-    return
+    workspace.showMessage(
+      "intelephense-server not found!, please run yarn global add intelephense-server",
+      "error"
+    );
+    return;
   }
 
-  const selector: DocumentSelector = [{
-    language: 'php',
-    scheme: 'file'
-  }]
+  const selector: DocumentSelector = [
+    {
+      language: "php",
+      scheme: "file"
+    }
+  ];
 
   let serverOptions: ServerOptions = {
     module: file,
-    args: ['--node-ipc'],
+    args: ["--node-ipc"],
     transport: TransportKind.ipc,
     options: {
       cwd: workspace.root,
       execArgv: config.execArgv || []
     }
-  }
+  };
 
-  let fsWatcher: FileSystemWatcher = workspace.createFileSystemWatcher('**/*.php', true, false, true)
+  let fsWatcher: FileSystemWatcher = workspace.createFileSystemWatcher(
+    "**/*.php",
+    true,
+    false,
+    true
+  );
 
   let clientOptions: LanguageClientOptions = {
     documentSelector: selector,
@@ -44,38 +74,69 @@ export async function activate(context: ExtensionContext): Promise<void> {
       configurationSection: sections,
       fileEvents: fsWatcher
     },
-    outputChannelName: 'php',
-    initializationOptions: {}
-  }
+    outputChannelName: "php",
+    initializationOptions: {},
+    middleware: {
+      provideCompletionItem: (
+        document: TextDocument,
+        position: Position,
+        context: CompletionContext,
+        token: CancellationToken,
+        next: ProvideCompletionItemsSignature
+      ): ProviderResult<CompletionItem[] | CompletionList> => {
+        return Promise.resolve(next(document, position, context, token)).then(
+          (res: CompletionItem[] | CompletionList) => {
+            let doc = workspace.getDocument(document.uri);
+            if (!doc) return [];
 
-  let client = new LanguageClient('phpls', 'PHP Language Server', serverOptions, clientOptions)
+            if (res.hasOwnProperty('isIncomplete')) {
+              let itemList = (res as CompletionList);
+              itemList.items.forEach(fixItem);
+              return itemList;
+            }
 
-  subscriptions.push(
-    services.registLanguageClient(client)
-  )
+            let items = (res as CompletionItem[]);
+            items.forEach(fixItem);
+            return items;
+          }
+        );
+      }
+    }
+  };
+
+  let client = new LanguageClient(
+    "phpls",
+    "PHP Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  subscriptions.push(services.registLanguageClient(client));
 
   client.onReady().then(async () => {
-    WorkspaceDiscovery.client = client
+    WorkspaceDiscovery.client = client;
 
     fsWatcher.onDidDelete(onDidDelete);
     fsWatcher.onDidCreate(onDidCreate);
     fsWatcher.onDidChange(onDidChange);
 
-    let startedTime: Date
+    let startedTime: Date;
 
     return await readAllFile(workspace.rootPath)
       .then(files => files.map(file => Uri.file(file)))
       .then(uriArray => {
         let token: CancellationToken;
-        workspace.showMessage('Indexing started.');
-        startedTime = new Date()
+        workspace.showMessage("Indexing started.");
+        startedTime = new Date();
         return WorkspaceDiscovery.checkCacheThenDiscover(uriArray, true, token);
       })
       .then(() => {
-        let usedTime: number = Math.abs(new Date().getTime() - startedTime.getTime())
+        let usedTime: number = Math.abs(
+          new Date().getTime() - startedTime.getTime()
+        );
         workspace.showMessage("Indexed php files, times: " + usedTime + "ms");
-      })
-  })
+      });
+  });
 }
 
 function onDidDelete(uri: Uri) {
@@ -94,9 +155,15 @@ function readAllFile(root: string) {
   return new Promise<string[]>((resolve, reject) => {
     Glob(root + "/**/*.php", (err, matches) => {
       if (err == null) {
-        resolve(matches)
+        resolve(matches);
       }
-      reject(err)
-    })
+      reject(err);
+    });
   });
+}
+
+function fixItem(item: CompletionItem): void {
+  if (/^\\\w+/.test(item.insertText) && !/^(\\\w+){2,}/.test(item.insertText)) {
+    item.insertText = item.insertText.replace('\\', '');
+  }
 }
