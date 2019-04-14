0e4c1f322e181b6a5ae54e2482c601c2b0a585b6
Mihael Dimoski <dimoskimihael@gmail.com>
2019-04-14 09:23:56 +0200

    Formatting

--- src/embeddedContentUri.ts
+++ src/embeddedContentUri.ts
@@ -3,20 +3,20 @@ import Uri from 'vscode-uri'
 export const EMBEDDED_CONTENT_SCHEME = 'embedded-content';
 
 export function isEmbeddedContentUri(virtualDocumentUri: Uri): boolean {
-	return virtualDocumentUri.scheme === EMBEDDED_CONTENT_SCHEME;
+    return virtualDocumentUri.scheme === EMBEDDED_CONTENT_SCHEME;
 }
 
 export function getEmbeddedContentUri(parentDocumentUri: string, embeddedLanguageId: string): Uri {
-	let uriString = EMBEDDED_CONTENT_SCHEME + '://' + embeddedLanguageId + '/' + encodeURIComponent(parentDocumentUri) + '.' + embeddedLanguageId;
-	return Uri.parse(uriString);
+    let uriString = EMBEDDED_CONTENT_SCHEME + '://' + embeddedLanguageId + '/' + encodeURIComponent(parentDocumentUri) + '.' + embeddedLanguageId;
+    return Uri.parse(uriString);
 };
 
 export function getHostDocumentUri(virtualDocumentUri: Uri): string {
-	let languageId = virtualDocumentUri.authority;
-	let path = virtualDocumentUri.path.substring(1, virtualDocumentUri.path.length - languageId.length - 1); // remove leading '/' and new file extension
-	return path;
+    let languageId = virtualDocumentUri.authority;
+    let path = virtualDocumentUri.path.substring(1, virtualDocumentUri.path.length - languageId.length - 1); // remove leading '/' and new file extension
+    return path;
 };
 
 export function getEmbeddedLanguageId(virtualDocumentUri: Uri): string {
-	return virtualDocumentUri.authority;
+    return virtualDocumentUri.authority;
 }
