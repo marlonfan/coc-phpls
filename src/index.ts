a7b1115d1cb77e662a293029e18b1f1666c91494
alalfakawma <alalfakawma@live.com>
2022-05-13 11:26:04 +0530

    fix: Fixed #104 and updated coc.nvim dependency. Removed unused vars, ran prettier, updated deprecated code.

--- src/index.ts
+++ src/index.ts
@@ -1,208 +1,220 @@
 import {
-    ExtensionContext,
-    LanguageClient,
-    ServerOptions,
-    workspace,
-    services,
-    TransportKind,
-    LanguageClientOptions,
-    CompletionContext,
-    ProvideCompletionItemsSignature,
-    ProviderResult,
-    commands
+  ExtensionContext,
+  LanguageClient,
+  ServerOptions,
+  workspace,
+  window,
+  TransportKind,
+  LanguageClientOptions,
+  CompletionContext,
+  ProvideCompletionItemsSignature,
+  ProviderResult,
+  commands,
 } from "coc.nvim";
 
 import {
-    CompletionItem,
-    TextDocument,
-    Position,
-    CompletionList,
-    NotificationType,
-    RequestType,
-    Disposable,
-    Definition
+  CompletionItem,
+  TextDocument,
+  Position,
+  CompletionList,
+  NotificationType,
+  RequestType,
+  Disposable,
 } from "vscode-languageserver-protocol";
 
 import { CancellationToken } from "vscode-jsonrpc";
-import { ProvideImplementationSignature } from 'coc.nvim/lib/language-client/implementation';
-import { Location } from 'vscode-languageserver-types';
-import { isArray } from 'util';
-import { existsSync  } from "fs";
+import { existsSync } from "fs";
 
-const LanguageID = 'php';
+const LanguageID = "php";
 
-const INDEXING_STARTED_NOTIFICATION = new NotificationType('indexingStarted');
-const INDEXING_ENDED_NOTIFICATION = new NotificationType('indexingEnded');
-const INDEX_WORKSPACE_REQUEST = new RequestType('indexWorkspace');
-const CANCEL_INDEXING_REQUEST = new RequestType('cancelIndexing');
+const INDEXING_STARTED_NOTIFICATION = new NotificationType("indexingStarted");
+const INDEXING_ENDED_NOTIFICATION = new NotificationType("indexingEnded");
+const CANCEL_INDEXING_REQUEST = new RequestType("cancelIndexing");
 
 let languageClient: LanguageClient;
 let extensionContext: ExtensionContext;
-let clientDisposable:Disposable;
+let clientDisposable: Disposable;
 let file: string;
 let licenceKey: string;
 
 export async function activate(context: ExtensionContext): Promise<void> {
-    extensionContext = context;
+  extensionContext = context;
 
-    let c = workspace.getConfiguration();
-    const config = c.get("phpls") as any;
-    const intelephenseConfig = c.get("intelephense") as any;
-    const enable = config.enable;
-    licenceKey = intelephenseConfig.licenceKey || '';
+  let c = workspace.getConfiguration();
+  const config = c.get("phpls") as any;
+  const intelephenseConfig = c.get("intelephense") as any;
+  const enable = config.enable;
+  licenceKey = intelephenseConfig.licenceKey || "";
 
-    if (enable === false) return;
-    if (existsSync(config.path)) {
-        try {
-            file = require.resolve(config.path);
-            if (file.endsWith("intelephense.js") === false) throw new Error();
+  if (enable === false) return;
+  if (existsSync(config.path)) {
+    try {
+      file = require.resolve(config.path);
+      if (file.endsWith("intelephense.js") === false) throw new Error();
 
-            /* ---- See :CocOpenLog ---- */
-            extensionContext.logger.info(
-                "intelephense module (phpls.path) is ready to be started"
-            );
-        } catch (e) {
-            workspace.showMessage(
-                "intelephense module not found! phpls.path is invalid.",
-                "error"
-            );
-            return;
-        }
-    } else {
-        try {
-            file = require.resolve("intelephense");
-            if (file.endsWith("intelephense.js") === false) throw new Error();
-
-            /* ---- See :CocOpenLog ---- */
-            extensionContext.logger.info(
-                "intelephense module (builtin) is ready to be started"
-            );
-        } catch (e) {
-            workspace.showMessage(
-                "intelephense module not found! builtin module is invalid",
-                "error"
-            );
-            return;
-        }
+      /* ---- See :CocOpenLog ---- */
+      extensionContext.logger.info(
+        "intelephense module (phpls.path) is ready to be started"
+      );
+    } catch (e) {
+      window.showMessage(
+        "intelephense module not found! phpls.path is invalid.",
+        "error"
+      );
+      return;
     }
+  } else {
+    try {
+      file = require.resolve("intelephense");
+      if (file.endsWith("intelephense.js") === false) throw new Error();
 
-    languageClient = createClient(context, false);
+      /* ---- See :CocOpenLog ---- */
+      extensionContext.logger.info(
+        "intelephense module (builtin) is ready to be started"
+      );
+    } catch (e) {
+      window.showMessage(
+        "intelephense module not found! builtin module is invalid",
+        "error"
+      );
+      return;
+    }
+  }
 
-    let indexWorkspaceDisposable = commands.registerCommand('intelephense.index.workspace', indexWorkspace);
-    let cancelIndexingDisposable = commands.registerCommand('intelephense.cancel.indexing', cancelIndexing);
+  languageClient = createClient(context, false);
 
-    context.subscriptions.push(
-        indexWorkspaceDisposable,
-        cancelIndexingDisposable,
-    );
+  let indexWorkspaceDisposable = commands.registerCommand(
+    "intelephense.index.workspace",
+    indexWorkspace
+  );
+  let cancelIndexingDisposable = commands.registerCommand(
+    "intelephense.cancel.indexing",
+    cancelIndexing
+  );
 
-    clientDisposable = languageClient.start();
+  context.subscriptions.push(
+    indexWorkspaceDisposable,
+    cancelIndexingDisposable
+  );
+
+  clientDisposable = languageClient.start();
 }
 
 function fixItem(item: CompletionItem): void {
-    if (/^\\\w+/.test(item.insertText) && !/^(\\\w+){2,}/.test(item.insertText)) {
-        item.insertText = item.insertText.replace('\\', '');
-    }
+  if (/^\\\w+/.test(item.insertText) && !/^(\\\w+){2,}/.test(item.insertText)) {
+    item.insertText = item.insertText.replace("\\", "");
+  }
 }
 
 function createClient(context: ExtensionContext, clearCache: boolean) {
-    // The debug options for the server
-    let debugOptions = {
-        execArgv: ["--nolazy", "--inspect=6039", "--trace-warnings", "--preserve-symlinks"],
-        detached: true
-    };
+  // The debug options for the server
+  let debugOptions = {
+    execArgv: [
+      "--nolazy",
+      "--inspect=6039",
+      "--trace-warnings",
+      "--preserve-symlinks",
+    ],
+    detached: true,
+  };
 
-    let serverOptions: ServerOptions = {
-        run: { module: file, transport: TransportKind.ipc },
-        debug: { module: file, transport: TransportKind.ipc, options: debugOptions }
-    }
+  let serverOptions: ServerOptions = {
+    run: { module: file, transport: TransportKind.ipc },
+    debug: {
+      module: file,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
 
-    // todo: implements createMiddleware method
-    // let middleware = createMiddleware(() => {
-    // 	return languageClient;
-    // });
+  // todo: implements createMiddleware method
+  // let middleware = createMiddleware(() => {
+  // 	return languageClient;
+  // });
 
-    let clientOptions: LanguageClientOptions = {
-        documentSelector: [
-            { language: LanguageID, scheme: 'file' },
-            { language: LanguageID, scheme: 'untitled' }
-        ],
-        initializationOptions: {
-            globalStoragePath: context.storagePath,
-            storagePath: context.storagePath,
-            clearCache: clearCache,
-            licenceKey: licenceKey,
-        },
-        middleware: {
-            provideCompletionItem: (
-                document: TextDocument,
-                position: Position,
-                context: CompletionContext,
-                token: CancellationToken,
-                next: ProvideCompletionItemsSignature
-            ): ProviderResult<CompletionItem[] | CompletionList> => {
-                return Promise.resolve(next(document, position, context, token)).then(
-                    (res: CompletionItem[] | CompletionList) => {
-                        let doc = workspace.getDocument(document.uri);
-                        if (!doc) return [];
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      { language: LanguageID, scheme: "file" },
+      { language: LanguageID, scheme: "untitled" },
+    ],
+    initializationOptions: {
+      globalStoragePath: context.storagePath,
+      storagePath: context.storagePath,
+      clearCache: clearCache,
+      licenceKey: licenceKey,
+    },
+    middleware: {
+      provideCompletionItem: (
+        document: TextDocument,
+        position: Position,
+        context: CompletionContext,
+        token: CancellationToken,
+        next: ProvideCompletionItemsSignature
+      ): ProviderResult<CompletionItem[] | CompletionList> => {
+        return Promise.resolve(next(document, position, context, token)).then(
+          (res: CompletionItem[] | CompletionList) => {
+            let doc = workspace.getDocument(document.uri);
+            if (!doc) return [];
 
-                        if (res.hasOwnProperty('isIncomplete')) {
-                            let itemList = (res as CompletionList);
-                            if (Array.isArray(itemList.items)) {
-                                itemList.items.forEach(fixItem);
-                            }
-                            return itemList;
-                        }
-
-                        let items = (res as CompletionItem[]);
-                        if (Array.isArray(items)) {
-                            items.forEach(fixItem);
-                        }
-                        return items;
-                    }
-                );
+            if (res) {
+              if (res.hasOwnProperty("isIncomplete")) {
+                let itemList = res as CompletionList;
+                if (Array.isArray(itemList.items)) {
+                  itemList.items.forEach(fixItem);
+                }
+                return itemList;
+              }
             }
-        },
-    };
 
-    languageClient = new LanguageClient(
-        "phpls",
-        "PHP Language Server",
-        serverOptions,
-        clientOptions
-    );
+            let items = res as CompletionItem[];
+            if (Array.isArray(items)) {
+              items.forEach(fixItem);
+            }
+            return items;
+          }
+        );
+      },
+    },
+  };
 
-    languageClient.onReady().then(() => {
-        let startedTime: Date;
+  languageClient = new LanguageClient(
+    "phpls",
+    "PHP Language Server",
+    serverOptions,
+    clientOptions
+  );
 
-        languageClient.onNotification(INDEXING_STARTED_NOTIFICATION.method, () => {
-            startedTime = new Date();
-            workspace.showMessage('intelephense indexing ...');
-        });
+  languageClient.onReady().then(() => {
+    let startedTime: Date;
 
-        languageClient.onNotification(INDEXING_ENDED_NOTIFICATION.method, () => {
-            let usedTime: number = Math.abs(
-                new Date().getTime() - startedTime.getTime()
-            );
-            workspace.showMessage("Indexed php files, times: " + usedTime + "ms");
-        });
+    languageClient.onNotification(INDEXING_STARTED_NOTIFICATION.method, () => {
+      startedTime = new Date();
+      window.showMessage("intelephense indexing ...");
     });
 
-    return languageClient;
+    languageClient.onNotification(INDEXING_ENDED_NOTIFICATION.method, () => {
+      let usedTime: number = Math.abs(
+        new Date().getTime() - startedTime.getTime()
+      );
+      window.showMessage("Indexed php files, times: " + usedTime + "ms");
+    });
+  });
+
+  return languageClient;
 }
 
-function indexWorkspace(licenceKey: string) {
-	if(!languageClient) {
-		return;
-    }
+function indexWorkspace(_licenceKey: string) {
+  if (!languageClient) {
+    return;
+  }
 
-	languageClient.stop().then(_ => {
-		clientDisposable.dispose();
-		languageClient = createClient(extensionContext, true);
-		clientDisposable = languageClient.start();
-	});
+  languageClient.stop().then((_) => {
+    clientDisposable.dispose();
+    languageClient = createClient(extensionContext, true);
+    clientDisposable = languageClient.start();
+  });
 }
 
 function cancelIndexing() {
-	languageClient.sendRequest(CANCEL_INDEXING_REQUEST.method);
+  languageClient.sendRequest(CANCEL_INDEXING_REQUEST.method);
 }
